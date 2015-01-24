61b90e6951c3c3ab5cd6b8745433d1d5b738e42e
banana-joe <edu.h@gmx.net>
2015-01-24 15:46:10 +0100

    [sCMS] + rainTPL template Engine

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+// + rainTPL template engine
+require_once "include/rain.tpl.class.php";
+raintpl::$tpl_dir = "templates/";
+raintpl::$cache_dir = "tmp/";
+
+if (isset($_GET['p']))
+{
+    switch ($_GET['p'])
+    {
+        // Platzhalter
+    }
+}
+?>
